299107ca64bacb97515e79fe5962d6c983605ce9
石塚直樹 <d.r-1004@i.softbank.jp>
2024-02-01 23:44:04 +0900

    abc334-a

---
+++ atcoder/abc334/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+using namespace std;
+
+int main() {
+    int b, g;
+    cin >> b >> g;
+    if (b > g) {
+        cout << "Bat" << endl;
+    } else {
+        cout << "Glove" << endl;
+    }
+    return 0;
+}
