33bf611c887e48263aa63eb1772779f2eb087c37
石塚直樹 <d.r-1004@i.softbank.jp>
2024-02-07 12:12:34 +0900

    abc331-a

---
+++ atcoder/abc331/a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+using namespace std;
+
+int main() {
+    int M, D;
+    int y, m, d;
+    cin >> M >> D;
+    cin >> y >> m >> d;
+    
+    return 0;
+}
