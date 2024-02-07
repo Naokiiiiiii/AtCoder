c98d91bc14cee96a2e011d819fe69458643f0259
石塚直樹 <d.r-1004@i.softbank.jp>
2024-02-07 12:15:14 +0900

    abc331-a

--- atcoder/abc331/a.cpp
+++ atcoder/abc331/a.cpp
@@ -8,6 +8,17 @@ int main() {
     int y, m, d;
     cin >> M >> D;
     cin >> y >> m >> d;
-    
+    int ansD, ansM, ansY;
+    if (d+1 > D) {
+        ansD = 1;
+        m += 1;
+    }
+    if (m > M) {
+        ansM = 1;
+        ansY = y+1;
+    } else {
+        ansY = y;
+    }
+    cout << ansY << " " << ansM << " " << ansD;
     return 0;
 }
